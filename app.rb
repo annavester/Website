d7895b2a232a419f4f6e1374df9f882191502081
Anna Vester <veannaa@gmail.com>
2012-04-14 23:19:39 -0500

    Added/configured compass

--- app.rb
+++ app.rb
@@ -1,8 +1,22 @@
 require 'rubygems'
+require 'compass'
 require 'sinatra'
 require 'haml'
 
+set :app_file, __FILE__
+set :root, File.dirname(__FILE__)
+set :public_folder, 'static'
+set :views, "views"
+
+configure do
+  Compass.add_project_configuration(File.join(Sinatra::Application.root, 'config', 'compass.config'))
+end
+
+get '/css/:name.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  sass(:"stylesheets/#{params[:name]}", Compass.sass_engine_options )
+end
+
 get '/' do
   haml :index
-  #'This Website is being upgraded'
 end
